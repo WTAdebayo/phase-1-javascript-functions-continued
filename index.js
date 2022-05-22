fa8993a876e5ac109400606a8d155499d9c0980b
Adewole Adebayo <wole1105@gmail.com>
2022-05-22 11:46:26 -0700

    commit tree e8a9a33f94e625d338deb38a4aa5d092d459093b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("bathe my dog");
+
+const mondayWork = function(location = 'go to the office') {
+    return (`This Monday, I will ${location}.`);
+}
+mondayWork('go to work');
+
+function wrapAdjective(para1="*") {
+    return function(para2="special") {
+        return `You are ${para1}${para2}${para1}!`;
+    };
+}
